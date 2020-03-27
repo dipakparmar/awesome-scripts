c90f4ac4d2a0ae296f9a93429be81f717f14fffb
dipakparmar <hi@dipak.tech>
2020-03-27 16:24:23 -0700

    Added docker install script

---
+++ install_docker.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "Runnning system package update check..."
+
+apt-get update
+
+echo "Runnning system package upgrade if any..."
+
+apt upgrade -y
+
+echo "Downloading latest docker script from https://get.docker.com"
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+echo "Running get-docker.sh script..."
+
+sudo sh get-docker.sh
+
+echo "Adding current user to docker group"
+
+sudo usermod -aG docker $USER
+
